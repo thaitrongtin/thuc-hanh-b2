1f20ad7148fe4180f20b8c1752023fb7e8b76e3b
TrongTin <trongtin812@gmail.com>
2023-09-19 13:58:49 +0700

    call api Jquery Ajax

---
+++ test1.js
@@ -0,0 +1,12 @@
+const urlBacon = "http://localhost:3000/posts";
+        (function(){$.ajax({           method: "GET",
+            url: urlBacon,
+            dataType: "json"
+        })
+            .done(function (data) {
+                console.log(data);
+            })
+            .fail(function () {
+                alert("no good");
+            });
+        })()
