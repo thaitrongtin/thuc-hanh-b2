d389e2c1a2b8e1b9e26f32b76831f562c3951be2
unknown <thaitrongtindev@gmail.com>
2023-09-25 21:50:56 +0700

    rest api method post

---
+++ test4.js
@@ -0,0 +1,18 @@
+var url = "https://my-json-server.typicode.com/typicode/demo/posts";
+var data = {};
+data.firstname = "Trong";
+data.lastname = "Tin";
+var json = JSON.stringify(data);
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+xhr.onload = function () {
+  var users = JSON.parse(xhr.responseText);
+  if (xhr.readyState == 4 && xhr.status == "201") {
+    console.table(users);
+  } else {
+    console.error(users);
+  }
+};
+xhr.send(json);
